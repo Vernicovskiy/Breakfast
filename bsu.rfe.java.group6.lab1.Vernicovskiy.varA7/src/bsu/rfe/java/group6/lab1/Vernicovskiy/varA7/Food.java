cf8562cb92757423d1ea2560b1769e39ba018bae
Vernicovskiy <victor02vernicovskiy@gmail.com>
2021-11-03 17:33:13 +0300

    Create class Food

---
+++ bsu.rfe.java.group6.lab1.Vernicovskiy.varA7/src/bsu/rfe/java/group6/lab1/Vernicovskiy/varA7/Food.java
@@ -0,0 +1,28 @@
+package bsu.rfe.java.group6.lab1.Vernicovskiy.varA7;
+
+public abstract class Food implements Consumable {
+    private String name;
+    public String getName(){
+        return name;
+    }
+
+    public Food(String name){ //описывает ссылку на экземпляр того объекта, чей код выполняется
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
+
+    public boolean equals(Object obj){
+        if(!(obj instanceof Food)){
+            return false;
+        }
+        else if (name == null || ((Food)obj).name == null){  // имя не задано
+            return false;
+        }
+        return name.equals(((Food)obj).name);
+    }
+
+}
+
